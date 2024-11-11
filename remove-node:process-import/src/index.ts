d87d97dfd44c8aa5bce9514b94d72ebb5d294029
lovelindhoni <jblovelindhoni@gmail.com>
2024-11-11 12:58:53 +0530

    fix: prevent newline from removing import

--- remove-node:process-import/src/index.ts
+++ remove-node:process-import/src/index.ts
@@ -4,13 +4,15 @@ export async function workflow({ files }: Api) {
   await files("**/*.{js,ts,tsx,jsx,cjs,mjs}")
     .jsFam()
     .astGrep({
+      id: "remove-node:process",
+      language: "TypeScript",
       rule: {
         pattern: {
           context: "import process from 'node:process'",
           strictness: "relaxed",
-        },
+        }
       },
+      fix: ""
     })
-    .replace(" ");
 }
 
